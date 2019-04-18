6bc43fdb8542add655de25c87008c497e967cf9d
addovej <addovej@gmail.com>
2019-04-18 07:23:51 +0300

    implemented embed terminal for accessing by SSH.

--- templates/static/js/routes.tsx
+++ templates/static/js/routes.tsx
@@ -3,8 +3,8 @@ import {Route, Switch, HashRouter} from 'react-router-dom';
 import {App} from './app';
 import FilesPage from './components/files/page';
 import LogsPage from './components/files/logPage';
+import XTerminal from "./components/terminal";
 // import Home from './components/home';
-// import XTerminal from "./components/Terminal";
 
 export const AppRouter: React.FunctionComponent<{}> = () => {
     return (
@@ -15,7 +15,7 @@ export const AppRouter: React.FunctionComponent<{}> = () => {
                     {/*<Route path="/" component={Home}/>*/}
                     <Route path="/files" component={FilesPage}/>
                     <Route path="/logs" component={LogsPage}/>
-                    {/*<Route path="/terminal" component={XTerminal} />*/}
+                    <Route path="/terminal" component={XTerminal} />
                 </Switch>
             </div>
         </HashRouter>
